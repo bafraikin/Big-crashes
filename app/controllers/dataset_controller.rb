89e74d93e0769bb5eb16487da2e7f1a7b44d50e7
bafraikin <fraikin.baptiste@gmail.com>
2018-10-01 16:00:45 +0200

    I finished the visual of dataset index

--- app/controllers/dataset_controller.rb
+++ app/controllers/dataset_controller.rb
@@ -1,7 +1,10 @@
 class DatasetController < ApplicationController
   def crashes
 
-    crashs = Dataset.find(params[:name]).crashes.map { | crash | [crash.date, crash.localisation, crash.source.name, crash.identifier]}
+
+
+    @category = Dataset.find(params[:name]).name
+    crashs = Dataset.find(params[:name]).crashes.map { | crash | [crash.date, crash.localisation, crash.identifier]}
     @by_years = {}
     crashs.each do |crash|
       @by_years[crash[0].strftime("%Y")] = [] if @by_years[crash[0].strftime("%Y")] == nil
