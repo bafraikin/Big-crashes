cd7b0c390a9361e0839435913f46f62cf82b0c36
bafraikin <fraikin.baptiste@gmail.com>
2018-10-01 15:27:27 +0200

    improving a bit the front

--- app/controllers/dataset_controller.rb
+++ app/controllers/dataset_controller.rb
@@ -1,5 +1,11 @@
 class DatasetController < ApplicationController
   def crashes
-    @crashs = Dataset.find(params[:name]).crashes.map { | crash |  [crash.date, crash.localisation, crash.source.name, crash.identifier]}
+
+    crashs = Dataset.find(params[:name]).crashes.map { | crash | [crash.date, crash.localisation, crash.source.name, crash.identifier]}
+    @by_years = {}
+    crashs.each do |crash|
+      @by_years[crash[0].strftime("%Y")] = [] if @by_years[crash[0].strftime("%Y")] == nil
+      @by_years[crash[0].strftime("%Y")] << crash
+    end
   end
 end
